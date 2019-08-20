4520a8b398910420b13f832a881717593844ce9e
zhljing <zhljing@rxhui.com>
2019-08-20 13:51:48 +0800

    修改域名匹配

--- src/lib/hostUtil.js
+++ src/lib/hostUtil.js
@@ -5,9 +5,9 @@
  */
 export function getDownloadHost () {
   var host = 'http://download.zq88.cn'
-  if (window.location.host === 'http://e-admin.rxhui.com') {
+  if (window.location.host === 'e-admin.rxhui.com') {
     host = 'http://download.zq88.cn'
-  } else if (window.location.host === 'http://10.0.0.105:10007') {
+  } else if (window.location.host === '10.0.0.105:10007') {
     host = 'http://10.0.0.112'
   } else {
     host = 'http://10.0.0.112'
@@ -22,9 +22,9 @@ export function getDownloadHost () {
  */
 export function getH5PageHost () {
   var host = 'https://moblie.hczq.com'
-  if (window.location.host === 'http://e-admin.rxhui.com') {
+  if (window.location.host === 'e-admin.rxhui.com') {
     host = 'https://moblie.hczq.com'
-  } else if (window.location.host === 'http://10.0.0.105:10007') {
+  } else if (window.location.host === '10.0.0.105:10007') {
     host = 'http://mobile-staging.jinhui001.com'
   } else {
     host = 'http://10.0.0.22:8088'
