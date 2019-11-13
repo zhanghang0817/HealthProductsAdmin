1606d7385506234a2a95c0adc1faba7a98a365a1
于子浩 <yzh_y59420@163.com>
2019-11-13 10:00:30 +0800

    1、按钮个数显示问题

--- src/views/homePop.vue
+++ src/views/homePop.vue
@@ -55,7 +55,7 @@
       width="100" align="center">
       <template slot-scope="scope">
          <span  v-if="scope.row.buttonOneLabel!= undefined && scope.row.buttonTwoLabel!= undefined ">2个</span>
-         <span  v-if="scope.row.buttonOneLabel!= undefined || scope.row.buttonTwoLabel!= undefined">1个</span>
+         <span  v-else-if="scope.row.buttonOneLabel!= undefined || scope.row.buttonTwoLabel!= undefined">1个</span>
          <span  v-else>--</span>
       </template>
       </el-table-column>
